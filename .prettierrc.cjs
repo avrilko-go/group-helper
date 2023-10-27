24e334593b1b00b099bc61f223c9cbe2069936b2
heb <heb@2345.com>
2023-10-27 14:43:41 +0800

    init

---
+++ .prettierrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  // 一行最多 120 字符
+  printWidth: 120,
+  // 使用 2 个空格缩进
+  tabWidth: 2,
+  // 结尾不需要分号
+  semi: false,
+  // 使用单引号
+  singleQuote: true,
+  // 箭头函数不需要括号
+  arrowParens: 'avoid',
+  // jsx 语法下是单引号
+  jsxSingleQuote: true,
+  // 对象的括号之间需要空格
+  bracketSpacing: true,
+  trailingComma: 'none',
+}
